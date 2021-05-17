fd708318be9afb064fcffef3fd9f29f0288c064d
Justin Mok <just.xyz@protonmail.com>
2021-05-17 11:02:03 -0700

    edited for interactions

--- src/commands/general/mcserver.ts
+++ src/commands/general/mcserver.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { CommandInteraction } from "discord.js";
 import { pingServer } from './utils/minecraft';
 
 // https://stackoverflow.com/a/106223
@@ -8,23 +8,27 @@ const ValidHostnameRegex = "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])
 module.exports = {
     name: 'mcserver',
     category: 'General',
-    usage: '[host]',
     description: 'Gets the status of a Minecraft server',
-    async execute(message: Message, args: string[]) {
-        if (args.length < 1) return;
-        
-        let host = args[0].split(':');
+    options: [{
+        type: 'STRING',
+        name: 'expression',
+        description: 'The IP address of the Minecraft server',
+        required: true
+    }],
+    async execute(interaction: CommandInteraction) {
+        let hostString = interaction.options[0].value! as string
+        let host = hostString.split(':');
         let port = parseInt(host[1]) || 25565;
 
         if (!(host[0].match(ValidIpAddressRegex))
             && !(host[0].match(ValidHostnameRegex)))
-            return message.channel.send(`\`${host[0]}\` is not a valid hostname or IP`);
+            return interaction.reply(`\`${host[0]}\` is not a valid hostname or IP`);
         
-        let statusMessage = await message.channel.send(`Pinging ${host[0]}`);
+        let statusMessage = await interaction.reply(`Pinging ${host[0]}`);
         pingServer(host[0], port).then(response => {
-            return statusMessage.edit([response.description.text, response.ping + ' ms', response.version.name].join('\n'));
+            return interaction.editReply([response.description.text, response.ping + ' ms', response.version.name].join('\n'));
         }).catch(err => {
-            return statusMessage.edit(`Response returned\n\`\`\`\n${err}\`\`\``);
+            return interaction.editReply(`Response returned\n\`\`\`\n${err}\`\`\``);
         });
     }
 }
